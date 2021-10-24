ffde8b1e982e49366645441c0f0da8f7c1e35c3e
priyankupadhya17 <priyankupadhya95@gmail.com>
2021-10-24 20:56:22 +0200

    created encoder-decoder model

--- arXiv_2006_03750v2_implementation/main.py
+++ arXiv_2006_03750v2_implementation/main.py
@@ -1,34 +1,36 @@
 from generate_graph import create_graph_dataset
 import torch
-#from arXiv_2006_03750v2_implementation.model import Model
+from model import Model
 from dataloader import GraphDataset
 from torch_geometric.loader import DataLoader
 
 
 def train():
     batch_size = 1
-    #device = 0 if torch.cuda.is_available() else 'cpu'
-    #model = Model().to(device)
+    device = 0 if torch.cuda.is_available() else 'cpu'
+    model = Model(hidden_dim=3, feature_dim=1).to(device)
 
-    #print(model)
+    print(model)
 
     tr_loader = GraphDataset()
     train_loader = DataLoader(tr_loader, batch_size=batch_size)
 
-    #optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     criterion = torch.nn.CrossEntropyLoss()
 
     for epochs in range(1):
         for batch, (graph, linegraph, mst) in enumerate(train_loader):
-            #model.train()
-            #optimizer.zero_grad()  # Clear gradients.
+            model.train()
+            optimizer.zero_grad()  # Clear gradients.
+            
+            linegraph.x = linegraph.features.view(linegraph.num_nodes,-1).type(torch.float)
             
             print(graph)
             print(linegraph)
             print(mst)
-
+            
+            out = model(linegraph.x, linegraph.edge_index)  # Perform a single forward pass.
             '''
-            out = model(x, edge_index_tensor)  # Perform a single forward pass.
             loss = criterion(out[train_mask], y[train_mask])  # Compute the loss solely based on the training nodes.
             loss.backward()  # Derive gradients.
             optimizer.step()  # Update parameters based on gradients.
--- arXiv_2006_03750v2_implementation/model.py
+++ arXiv_2006_03750v2_implementation/model.py
@@ -1,21 +1,61 @@
-from torch.nn import Linear, ReLU, Module
-from torch_geometric.nn import Sequential, GCNConv, TopKPooling
+from torch.nn import Linear, ReLU, Module, Sequential, Softmax
+from torch_geometric.nn import Sequential, GAT, GATConv
+import torch
+from torch.autograd import Variable
 
 
 class Model(Module):
-    def __init__(self):
+    def __init__(self, hidden_dim, feature_dim):
         super(Model, self).__init__()
+        
+        self.hidden_dim = hidden_dim
+        self.feature_dim = feature_dim
+        self.const = 1
+        
+        
+        #ENCODER
+        #self.gat_conv1 = GATConv(in_channels=1, out_channels=3, add_self_loops=True)
+        #self.gat_conv2 = GATConv(in_channels=3, out_channels=3, add_self_loops=True)
+        #self.gat_conv3 = GATConv(in_channels=3, out_channels=1, add_self_loops=True)
+        self.gat = GAT(in_channels=1, hidden_channels=3, num_layers=3, out_channels=1)
+        
+        self.phi1 = Variable(torch.randn(hidden_dim, feature_dim), requires_grad=True)
+        self.phi2 = Variable(torch.randn(hidden_dim, feature_dim), requires_grad=True)
+        
+    
+    def decoderFunc(self, starting_node, x, edge_index):
+        
+        edge_index_sources = torch.where(edge_index[0] == starting_node)
+        edge_index_dest = edge_index[1][edge_index_sources]
+        
+        alphas = (self.phi1 @ x[starting_node]).T @ (self.phi2 @ x[edge_index_dest].T)
+        alphas = alphas / torch.sqrt(torch.tensor(self.hidden_dim))
+        alphas = self.const * torch.tanh(alphas)
+        
+        alpha_probs = Softmax(dim=1)(alphas)
+        
+        next_starting_node = edge_index_dest[alpha_probs.argmax(dim=1)]
+        
+        return next_starting_node
 
-        self.conv1 = GCNConv(in_channels=1, out_channels=3)
-        self.conv2 = GCNConv(in_channels=3, out_channels=3)
-        self.conv3 = GCNConv(in_channels=3, out_channels=3)
-        self.linear = Linear(in_features=3, out_features=1)
-        self.relu = ReLU()
-
+    
     def forward(self, x, edge_index):
-        x = self.relu(self.conv1(x, edge_index))
-        x = self.relu(self.conv2(x, edge_index))
-        x = self.relu(self.conv3(x, edge_index))
-
+        
+        #x = self.gat_conv1(x, edge_index)
+        #x = self.gat_conv2(x, edge_index)
+        #x = self.gat_conv3(x, edge_index)
+        x = self.gat(x, edge_index)
+        x = Softmax(dim=0)(x)
+        print(x)
+        
+        n_nodes = x.shape[0]
+        starting_node = torch.argmax(x, dim=0)
+        
+        
+        for i in range(n_nodes):
+            print(starting_node)
+            starting_node = self.decoderFunc(starting_node, x, edge_index)
+        
+        
         return x
 
